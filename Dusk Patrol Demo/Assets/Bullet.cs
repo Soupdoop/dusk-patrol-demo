d00db6aa72ed684eea496d8dbc4271762918f239
Soupdoop <jherd23@gmail.com>
2017-08-28 22:19:04 -0400

    Bullet & Player

---
+++ Dusk Patrol Demo/Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float xspeed;
+	public float yspeed;
+
+	public float damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (xspeed, yspeed);
+	}
+
+	public void die() {
+		Destroy (gameObject);
+	}
+}
--- Dusk Patrol Demo/Assets/Player.cs
+++ Dusk Patrol Demo/Assets/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour {
 
 	public float speed;
 
+	public float health;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,6 +15,10 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
 
 		Vector2 movement = new Vector2(0,0);
 
@@ -29,5 +35,15 @@ public class Player : MonoBehaviour {
 			movement += new Vector2(1, 0);
 		}
 		GetComponent<Rigidbody2D> ().velocity = movement * speed;
+	}
+
+	void take(float d) {
+		health -= d;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Bullet bscript = other.gameObject.GetComponent<Bullet> ();
+		this.take (bscript.damage);
+		bscript.die ();
 	}
 }
